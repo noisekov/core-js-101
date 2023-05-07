276bba20170fbfeb40c57fde6d814dcdbec1c4c7
Vladimir <noise123q@mail.ru>
2023-05-07 22:45:16 +0300

    feat: solve isPrime

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -207,8 +207,23 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 1 || n === 2) {
+    return true;
+  }
+
+  let count = 0;
+  for (let i = 2; i <= n; i += 1) {
+    if (n % i === 0) {
+      count += 1;
+    }
+  }
+
+  if (count > 1) {
+    return false;
+  }
+
+  return true;
 }
 
 /**
