eb00539dabc141ae62ed898d8920a30014f7a4cf
Vladimir <noise123q@mail.ru>
2023-06-07 20:32:19 +0300

    feat: propagateItemsByPositionIndex

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -270,8 +270,11 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((acc, val, i) => {
+    acc.push(...Array.from({ length: i + 1 }).fill(val));
+    return acc;
+  }, []);
 }
 
 
