68db819cfa438e4bf013fd52e63755b27a9ab05d
Vladimir <noise123q@mail.ru>
2023-06-20 19:42:24 +0300

    feat: solve timeSpanToString, getComposition

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -80,8 +80,12 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const hour = `${Math.abs(startDate.getHours() - endDate.getHours())}`.padStart(2, '0');
+  const minute = `${Math.abs(startDate.getMinutes() - endDate.getMinutes())}`.padStart(2, '0');
+  const second = `${Math.abs(startDate.getSeconds() - endDate.getSeconds())}`.padStart(2, '0');
+  const getMilliseconds = `${Math.abs(startDate.getMilliseconds() - endDate.getMilliseconds())}`.padStart(3, '0');
+  return `${hour}:${minute}:${second}.${getMilliseconds}`;
 }
 
 
